79b85ddaf00e8e94ebd3f437232255294bfdb909
Serhiy Kalinets <kalinets@gmail.com>
2012-05-11 12:28:54 +0300

    renamed module file

---
+++ AddDefaultIgnoreFiles.psm1
@@ -0,0 +1,33 @@
+function Add-HgIgnore()
+{
+  (new-object Net.WebClient).DownloadString("https://raw.github.com/skalinets/add-default-ignores/master/Content/hgignore") > .hgignore
+<#
+.Synopsis
+    Adds VS optimized .hgignore to current directory.
+.Description 
+    Command will download default .hgignore file from github repository (https://raw.github.com/skalinets/add-default-ignores/master/Content/hgignore) and put it to the current folder. File contains patterns that usually should be ignored by Mercurial.
+.Link
+    https://github.com/skalinets/add-default-ignores
+.Example
+    Add-HgIgnore
+#>
+}
+
+function Add-GitIgnore()
+{
+  (new-object Net.WebClient).DownloadString("https://raw.github.com/skalinets/add-default-ignores/master/Content/gitignore") > .gitignore
+
+<#
+.Synopsis
+    Adds VS optimized .gitignore to current directory.
+.Description 
+    Command will download default .gitignore file from github repository (https://raw.github.com/skalinets/add-default-ignores/master/Content/gitignore) and put it to the current folder. File contains patterns that usually should be ignored by git.
+.Link
+    https://github.com/skalinets/add-default-ignores
+.Example
+    Add-GitIgnore
+#>
+
+}
+
+Export-ModuleMember Add-GitIgnore, Add-HgIgnore
